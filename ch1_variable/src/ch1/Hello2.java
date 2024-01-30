e0572c098e8ed7db5cc96bfa20499ed05fc97ad2
namjunhee <njh0581@naver.com>
2024-01-30 17:50:15 +0900

    클래스 작성

---
+++ ch1_variable/src/ch1/Hello2.java
@@ -0,0 +1,12 @@
+package ch1;
+
+public class Hello2 {
+    public static void main(String[] args) {
+        int var1 = 5;
+        System.out.println("내가 좋아하는 숫자는 "+var1);
+    }
+
+}
+//자바 기본단위 = 클래스
+//ch1 = 패키지(관련있는 클래스들 모음)
+//main() = 메소드
